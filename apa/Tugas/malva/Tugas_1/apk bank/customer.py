259bf731e145626a2eef361ae76c592f697f53be
malvarnaa <malvriski@gmail.com>
2023-04-10 15:08:43 +0700

    malva

---
+++ apa/Tugas/malva/Tugas_1/apk bank/customer.py
@@ -0,0 +1,98 @@
+def cekUser(users, username):
+    for u in users:
+        if u['username'] == username :
+            return False
+    return True
+
+def cekTo(account, to):
+    for a in range(len(account)):
+        if account[a]['username'] == to:
+            return True
+    return False
+
+
+
+def showBalance(account, username):
+    for a in account:
+        if a['username'] == username:
+            print('Your Balance : ', str(a['amount']))
+
+def withdraw(account, transaction, username):
+    amount = input('Masukkan jumlah withdraw : ')
+    for a in range(len(account)):
+        if account[a]['username'] == username:
+            if account[a]['amount'] - int(amount) >= 0:
+                account[a]['amount'] -= int(amount)
+                transaction.append(
+                    {
+                        'username':username,
+                        'to': 'self',
+                        'amount': int(amount),
+                        'type': 'withdraw',
+                    }
+                )
+                print('Tarik tunai berhasil!')
+            else:
+                print('Maaf, Saldo anda tidak mencukupi!')
+
+def deposit(account, transaction, username):
+    amount = input('Masukkan jumlah deposit : ')
+    for a in range(len(account)):
+        if account[a]['username'] == username:
+            account[a]['amount'] += int(amount)
+            transaction.append(
+                {
+                    'username':username,
+                    'to': 'self',
+                    'amount': int(amount),
+                    'type': 'deposit',
+                }
+            )
+            print('Saldo berhasil di tambahkan!')
+
+def transfer(account, transaction, username):
+    amount = input('Masukkan jumlah transfer : ')
+    while True:
+        to = input('Masukkan nama tujuan : ')
+        if cekTo(account, to):
+            for a in range(len(account)):
+                if account[a]['username'] == username:
+                    if account[a]['amount'] - int(amount) >= 0:
+                        account[a]['amount'] -= int(amount)
+                        for a in range(len(account)):
+                            if account[a]['username'] == to:
+                                account[a]['amount'] += int(amount)
+                                transaction.append(
+                                    {
+                                        'username':username,
+                                        'to': to,
+                                        'amount': int(amount),
+                                        'type': 'transfer',
+                                    }
+                                )
+                                print('Transfer berhasil!')
+                                return True
+                    else:
+                        print('Saldo kamu tidak cukup!')
+                        return False
+        else:
+            print('Nama tujuan tidak tersedia!')
+                
+             
+def showTransaction(transaction, username):
+    for t in range(len(transaction)):
+        if transaction[t]['username'] == username:
+            i = 1
+            for t in range(len(transaction)):
+                if transaction[t]['username'] == username:
+                    print('')
+                    print('=== Transaksi ', str(i), '===')
+                    print('username : ', transaction[t]['username'])
+                    print('to : ', transaction[t]['to'])
+                    print('amount : ', transaction[t]['amount'])
+                    print('typr : ', transaction[t]['type'])
+                    print('')
+                i += 1
+            return True
+    print('You havent made any transactions!')
+           
